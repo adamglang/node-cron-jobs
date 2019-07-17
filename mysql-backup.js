215d0f823bfdc9adcd2e9592945425a61f1d56fa
Adam Lang <c.adam.lang@zonarsystems.com>
2019-07-16 19:35:25 -0700

    wrap all in try catch

--- mysql-backup.js
+++ mysql-backup.js
@@ -4,22 +4,22 @@ const moment = require('moment');
 const config = require('../config');
 
 async function mysqlBackup() {
-	const upload = s3Upload({
-		accessKeyId: config.aws.accessKey,
-		secretAccessKey: config.aws.secretKey,
-		Bucket: config.aws.buckets.backup.name,
-		region: config.aws.buckets.backup.region,
-	});
-
-	const s3 = upload({ Key: `mysql-backup-${moment().format('YYYY-MM-DD-HH-mm-ss')}.sql` });
-
-	const mysqldump = spawn('mysqldump', [
-		'-u', config.db.connection.user,
-		`-p ${config.db.connection.password}`,
-		config.db.connection.database,
-	]);
-
 	try {
+		const upload = s3Upload({
+			accessKeyId: config.aws.accessKey,
+			secretAccessKey: config.aws.secretKey,
+			Bucket: config.aws.buckets.backup.name,
+			region: config.aws.buckets.backup.region,
+		});
+
+		const s3 = upload({ Key: `mysql-backup-${moment().format('YYYY-MM-DD-HH-mm-ss')}.sql` });
+
+		const mysqldump = spawn('mysqldump', [
+			'-u', config.db.connection.user,
+			`-p ${config.db.connection.password}`,
+			config.db.connection.database,
+		]);
+
 		await mysqldump.stdout.pipe(s3);
 	} catch (e) {
 		console.error(e);
